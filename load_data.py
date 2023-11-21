d27bcf86ccad9547e849f0ea75c9717ad60c9578
CargoCultScientist <martin.sona@gmail.com>
2023-11-21 14:04:05 +0100

    Update load_data.py
    additional commenting

--- load_data.py
+++ load_data.py
@@ -1,3 +1,6 @@
+#this code is designed to fetch and store data from the Newscatcher API for multiple search terms related to disinformation. 
+#It saves the data as JSON files, adds a unique ID to each entry, and then loads and stores the data in an SQLite database. 
+#The code is organized into functions to facilitate the loading and storing of data for different search terms.
 
 #Import common  
 import json
@@ -18,6 +21,7 @@ for q in search_terms:
 
 
 # Deserialize and dump
+# This function adds a unique ID column to a DataFrame. It's used to give each row a unique identifier.
 def add_unique_id(df):
     """
     Add a unique ID column to the DataFrame.
@@ -25,6 +29,7 @@ def add_unique_id(df):
     df['unique_id'] = range(1, len(df) + 1)
     return df
 
+# This function loads a DataFrame into an SQLite database. It creates a database connection, creates a table (or replaces it if it already exists), and inserts the data from the DataFrame into the table.
 def load_data_df(df,db_name,table_name):
     # Step 3: Create SQLite DB
     conn = sqlite3.connect(db_name)
@@ -35,6 +40,7 @@ def load_data_df(df,db_name,table_name):
     conn.close()
     return 
 
+#  This function loads data from a JSON file into a DataFrame, adds a unique ID, and then stores it in an SQLite database using the load_data_df function.
 def load_data(filename,db_name,table_name):
     with open(filename, 'r') as f:
         json_data = json.load(f)
@@ -52,6 +58,7 @@ def load_data(filename,db_name,table_name):
     conn.close()
     return 
 
+# This function iterates through a list of filenames, loads each JSON file into a DataFrame, and stores it in the SQLite database using the load_data function.
 def mass_load(filenames,db_name,table_name):
     for name in filenames:
         load_data(name,db_name,table_name) 
