1d0463fe8445af0f11924f5b10ea5348cecce0b4
CargoCultScientist <martin.sona@gmail.com>
2023-11-21 14:38:31 +0100

    Update tocsv.py
    comments

--- tocsv.py
+++ tocsv.py
@@ -1,15 +1,16 @@
-# this converts a zst file to csv
-#
-# it's important to note that the resulting file will likely be quite large
-# and you probably won't be able to open it in excel or another csv reader
-#
-# arguments are inputfile, outputfile, fields
-# call this like
+# This Python script is designed to convert a .zst file (Zstandard compressed file) to a .csv (comma-separated values) file. 
+# It is specifically tailored for handling large datasets that are commonly found in data analysis tasks. 
+# The script takes three command-line arguments: the input file (a .zst file), the output file (a .csv file), and a list of fields to be extracted from the JSON objects in the .zst file.
+# The script uses the logging module to log information and errors, which is helpful for monitoring the progress and diagnosing issues during processing.
+# Errors like JSON decoding issues or missing keys in the JSON objects are logged, and processing continues.
+# This script is particularly useful in data analysis scenarios where large datasets are compressed for storage efficiency and need to be converted into a more accessible format like CSV for analysis.
+
+# call this like:
 # python to_csv.py wallstreetbets_submissions.zst wallstreetbets_submissions.csv author,selftext,title
 
-import zstandard
+import zstandard # zstandard library to decompress the .zst file. It reads the compressed data, decompresses it, and processes it in chunks.
 import os
-import json
+import json # Each line in the decompressed data is expected to be a JSON object. The script extracts specified fields from these JSON objects.
 import sys
 import csv
 from datetime import datetime
@@ -20,7 +21,7 @@ log = logging.getLogger("bot")
 log.setLevel(logging.DEBUG)
 log.addHandler(logging.StreamHandler())
 
-
+# This function reads chunks of the compressed file, attempting to decode them into a string.
 def read_and_decode(reader, chunk_size, max_window_size, previous_chunk=None, bytes_read=0):
 	chunk = reader.read(chunk_size)
 	bytes_read += chunk_size
@@ -33,7 +34,7 @@ def read_and_decode(reader, chunk_size, max_window_size, previous_chunk=None, by
 			raise UnicodeError(f"Unable to decode frame after reading {bytes_read:,} bytes")
 		return read_and_decode(reader, chunk_size, max_window_size, chunk, bytes_read)
 
-
+# Opens the .zst file and processes it in chunks. Each chunk is split into lines, and each line (presumably a JSON object) is yielded for processing.
 def read_lines_zst(file_name):
 	with open(file_name, 'rb') as file_handle:
 		buffer = ''
@@ -50,6 +51,10 @@ def read_lines_zst(file_name):
 			buffer = lines[-1]
 		reader.close()
 
+# main block: 
+# Handles command-line arguments, sets up the CSV writer, and iterates over lines from read_lines_zst. 
+# For each line, it attempts to parse it as JSON, extract the desired fields, and write them to the CSV file. 
+# It logs progress and handles errors like JSON decode errors or missing keys.
 
 if __name__ == "__main__":
 	input_file_path = sys.argv[1]
